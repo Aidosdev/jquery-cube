2f657f4719fca051b40ce5c7c3bf4d3c82cf1dac
Aidosdev <vip_aidos_96@mail.ru>
2020-11-07 14:06:04 +0600

    3d cube

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+// $('h3').css({'color': 'blue', 'font-size': '50px' });
+
+// $('.test').css('color', 'red');
+
+// $('h3.test').css('color', 'orange');
+
+
+// $(':header').css('color', 'violet');
+
+
+// $('h3:not(.test)').css('color', 'aqua');
+
+// $(':header + header').css('color', 'pink');
+
+
+
+
+
+
+
+=======================================================
+
+$('.cube').on('mouseout', function() {
+	$('.cube').toggleClass('asd');
+	$('.cube').toggleClass('asd1');
+	$('.cube').toggleClass('asd2');
+	$('.cube').toggleClass('asd3');
+});
+
+
+
